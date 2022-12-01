ccdbd50d3543b0656162dc5454456ba39281ebf2
Ozodbek <johnfreedom99600@gmail.com>
2022-12-01 11:36:30 +0500

    changed discountitem serializer validate method

--- discount/serializers.py
+++ discount/serializers.py
@@ -2,6 +2,7 @@ from django.db.models import Sum
 from rest_framework import serializers
 from django.db import transaction
 from account.serializers import UserSerializer
+from product.enums import DiscountType
 from product.models import ProductSize, Product
 from product.serializers import ProductForOrderWishlistSerializer
 from .models import Discount, DiscountItem
@@ -21,6 +22,7 @@ class DiscountSerializer(serializers.ModelSerializer):
                   )
 
 
+
 class DiscountItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = DiscountItem
@@ -31,3 +33,18 @@ class DiscountItemSerializer(serializers.ModelSerializer):
                     'product',
                     'is_active',
                     )
+
+    def validate(self, attrs):
+        type = attrs.get('type')
+        category = attrs.get('category')
+        product = attrs.get('product')
+        if type == DiscountType.all.value and (category or product):
+            raise serializers.ValidationError("Hamma tovar tanlanganda alohida kategory yoki tovar tanlash mumkin emas!")
+
+        if type == DiscountType.category.value and product or (type == DiscountType.category.value and category is None):
+            raise serializers.ValidationError("Kategoryni kiriting product emas!")
+
+        if type == DiscountType.product.value and category or (type == DiscountType.product.value and  product is None):
+            raise serializers.ValidationError("Product kiriting kategory emas!")
+
+        return attrs
